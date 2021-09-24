56c80b59016b66cadfb44ceb9909dccda09eddf5
smita <smitamore00@gmail.com>
2021-09-24 12:43:59 +0530

    variable added

--- proj1.java
+++ proj1.java
@@ -3,7 +3,8 @@ public class SumOfNumbers1
 public static void main(String args[])   
 {  
 int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+sum = n1 + n2;
+int m=0  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  
